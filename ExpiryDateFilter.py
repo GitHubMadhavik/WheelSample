069bc8097552c6fc9aa2a45aa6ac9ee58a680b89
Jose Martinez <GitHubMadhavik@users.noreply.github.com>
2021-02-16 17:59:42 -0600

    Update ExpiryDateFilter.py

--- ExpiryDateFilter.py
+++ ExpiryDateFilter.py
@@ -1,11 +1,10 @@
 import csv
 import pandas as pd
 
-read = input("Enter File Path: ")
-expirydate = input("Enter Expire Date: (mm/dd/yyyy)  ")
+date = input("Enter Date: Format(yyyy-mm-dd)        ")
 
-filtered = pd.read_csv(read, usecols=["Expiry"])
-dateresults = (filtered[filtered["Expiry"] > expirydate])
-results = dateresults.value_counts()
+filtered = pd.read_csv("C:\Script\PolicyData.csv", parse_dates=['Expiry'])
+filteredresults = filtered[(filtered['Expiry'] > date) & (filtered['Expiry'] <= '2050-01-01')]
+results = len(filteredresults)
 
 print(results)
