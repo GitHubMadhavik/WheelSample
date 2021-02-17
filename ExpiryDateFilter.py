c6124c66ff0793ef31050f8c39bf47c0b2e0769b
Jose Martinez <GitHubMadhavik@users.noreply.github.com>
2021-02-16 18:05:40 -0600

    Update ExpiryDateFilter.py

--- ExpiryDateFilter.py
+++ ExpiryDateFilter.py
@@ -4,7 +4,7 @@ import pandas as pd
 date = input("Enter Date: Format(yyyy-mm-dd)        ")
 
 filtered = pd.read_csv("C:\Script\PolicyData.csv", parse_dates=['Expiry'])
-filteredresults = filtered[(filtered['Expiry'] > date) & (filtered['Expiry'] <= '2050-01-01')]
+filteredresults = filtered[(filtered['Expiry'] > date) & (filtered['Expiry'] <= '2099-01-01')]
 results = len(filteredresults)
 
 print(results)
